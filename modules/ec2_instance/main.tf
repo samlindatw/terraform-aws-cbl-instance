cdac645be981d17fb8f87439e3dddadef6129ac9
Sky Huang <samlinda@MarsMBP.local>
2021-06-04 06:43:05 +0800

    first commit

---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ssm_parameter" "cathay-ami" {
+  name = "/aws/service/ami-amazon-linux-latest-amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "cathay-module-example" {
+  #use the value provided by
+  ami               = data.aws_ssm_parameter.cathay-ami.value
+  instance_type     = var.instance-type
+  availability_zone = var.instance-az
+  count             = var.instance-count
+
+  tags = {
+    Name = var.server-name
+  }
+}
