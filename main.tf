be5d8fd10d877f425a6d1d9847da996a269ee60b
Sky Huang <samlinda@MarsMBP.local>
2021-06-20 13:34:13 +0800

    first commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_ssm_parameter" "cathay-ami" {
   name = "/aws/service/ami-amazon-linux-latest-amzn2-ami-hvm-x86_64-gp2"
 }
 
-//new version
+//new version 2.0.0
 resource "aws_instance" "cathay-module-example" {
   #use the value provided by
   ami               = data.aws_ssm_parameter.cathay-ami.value
